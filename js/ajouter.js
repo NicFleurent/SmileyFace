0ba2875b433e5adb6bcc8b0a88b4ff28e7c81407
Nicolas Fleurent <nicolas.fleurent.02@edu.cegeptr.qc.ca>
2023-09-12 17:31:56 -0400

    Page Ajouter et nouvelle BD

---
+++ js/ajouter.js
@@ -0,0 +1,76 @@
+let nom = document.getElementById("nom");
+let date = document.getElementById("date");
+let lien = document.getElementById("lien");
+let departement = document.getElementById("departement");
+
+let invalidNom = document.getElementById("invalidNom");
+let invalidDate = document.getElementById("invalidDate");
+let invalidLien = document.getElementById("invalidLien");
+let invalidDepartement = document.getElementById("invalidDepartement");
+
+let inputNom;
+let inputDate;
+let inputLien;
+let inputDepartement;
+
+const regexURL = /\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i;
+
+nom.addEventListener("input", function(event){
+    inputNom = nom.value;
+    
+    if(inputNom === "" || inputNom === null){
+        invalidNom.style.display = "block";
+        nom.setAttribute("class", "form-control is-invalid");
+    }
+    else{
+        invalidNom.style.display = "none";
+        nom.setAttribute("class", "form-control is-valid");
+    }
+
+});
+
+date.addEventListener("input", function(event){
+    inputDate = date.value;
+    console.log(inputDate);
+
+    if(inputDate === "" || inputDate === null){
+        invalidDate.style.display = "block";
+        date.setAttribute("class", "form-control is-invalid");
+    }
+    else{
+        invalidDate.style.display = "none";
+        date.setAttribute("class", "form-control is-valid");
+    }
+});
+
+lien.addEventListener("input", function(event){
+    inputLien = lien.value;
+
+    if(inputLien === "" || inputLien === null){
+        invalidLien.style.display = "block";
+        invalidLien.innerHTML = "Le lien de l'évènement est requis";
+        lien.setAttribute("class", "form-control is-invalid");
+    }
+    else if(!(regexURL.test(inputLien))){
+        invalidLien.style.display = "block";
+        invalidLien.innerHTML = "L'URL n'est pas valide";
+        lien.setAttribute("class", "form-control is-invalid");
+    }
+    else{
+        invalidLien.style.display = "none";
+        lien.setAttribute("class", "form-control is-valid");
+    }
+});
+
+departement.addEventListener("input", function(event){
+    inputDepartement = departement.value;
+
+    if(inputDepartement === "" || inputDepartement === null){
+        invalidDepartement.style.display = "block";
+        departement.setAttribute("class", "form-control is-invalid");
+    }
+    else{
+        invalidDepartement.style.display = "none";
+        departement.setAttribute("class", "form-control is-valid");
+    }
+});
