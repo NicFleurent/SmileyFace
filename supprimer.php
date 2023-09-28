dba5e15163fca7a99f91a069bb17be4621f996ed
Nicolas Fleurent <1596189@techinfotr.qc.ca>
2023-09-28 16:37:03 -0400

    Finaliser bouton suppression, Navbar page validation

--- supprimer.php
+++ supprimer.php
@@ -3,18 +3,53 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    if($_SESSION['connexion'] == true){
+    if ($_SESSION['connexion'] == true) {
         $id  = "";
-        $idErreur = "";
+        $idErreur = $erreurSQL = "";
         $erreur = false;
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "smileyface";
+
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        if (isset($_GET['id'])) {
+            $id = test_input($_GET['id']);
+
+            $sql = "DELETE FROM evenement_departement WHERE id_evenement=" . $id;
+            if ($conn->query($sql) === TRUE) {
+                echo "Succes : Supprimer de evenement_departement<br>";
+            } else {
+                $erreurSQL = "Error: " . $sql . "<br>" . mysqli_error($conn);
+                $erreur = true;
+            }
+
+            $sql = "DELETE FROM evenement WHERE id=" . $id;
+            if ($conn->query($sql) === TRUE) {
+                header("Location: ./index.php?succes=supprimer");
+                die();
+            } else {
+                $erreurSQL = "Error: " . $sql . "<br>" . mysqli_error($conn);
+                $erreur = true;
+            }
+            $conn->close();
+        }
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -24,27 +59,16 @@ session_start();
             }
             $id = test_input($_POST["id"]);
 
-            if(!$erreur){
-                $servername = "localhost";
-                $username = "root";
-                $password = "root";
-                $dbname = "smileyface";
-
-                $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-
-                $sql = "DELETE FROM evenement_departement WHERE id_evenement=". $id;
+            if (!$erreur) {
+                $sql = "DELETE FROM evenement_departement WHERE id_evenement=" . $id;
                 if ($conn->query($sql) === TRUE) {
                     echo "Succes : Supprimer de evenement_departement<br>";
                 } else {
                     $erreurSQL = "Error: " . $sql . "<br>" . mysqli_error($conn);
                     $erreur = true;
-                } 
+                }
 
-                $sql = "DELETE FROM evenement WHERE id=". $id;
+                $sql = "DELETE FROM evenement WHERE id=" . $id;
                 if ($conn->query($sql) === TRUE) {
                     header("Location: ./index.php?succes=supprimer");
                     die();
@@ -54,22 +78,20 @@ session_start();
                 }
                 $conn->close();
             }
-            
         }
         if ($_SERVER["REQUEST_METHOD"] != "POST" || $erreur == true) {
 
             echo $erreurSQL;
             echo $idErreur;
-            if(!$erreur){
+            if (!$erreur) {
     ?>
-    <div>
-        Vous ne pouvez pas acceder directement à cette page
-    </div>
+                <div>
+                    Vous ne pouvez pas acceder directement à cette page
+                </div>
     <?php
             }
         }
-    }
-    else{
+    } else {
         header("Location: ./connexion.php");
     }
 
@@ -82,4 +104,5 @@ session_start();
     }
     ?>
 </body>
+
 </html>
