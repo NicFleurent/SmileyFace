a7121fa7147ce024a2dacc0d33d011d10a95fc7a
Mirolie10 <mirolie010@gmail.com>
2023-09-29 08:25:49 -0400

    total votes et titre event

--- statistique.php
+++ statistique.php
@@ -10,9 +10,6 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Statistiques</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/index.css">
-
 
 </head>
 
@@ -40,10 +37,10 @@ session_start();
                         </li>
                         <li class="nav-item ms-5">
                             <a class="btn btn-outline-light" href="deconnexion.php">Déconnexion <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
-                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
-                            </svg></a>
-                           
+                                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                                </svg></a>
+
                         </li>
 
                     </ul>
@@ -72,9 +69,9 @@ session_start();
 
             if (isset($_GET['id'])) {
                 $id = $_GET['id'];
-                
+
                 //string de requête
-                $sql = "SELECT etudiantSatisfait,etudiantNeutre,etudiantInsatisfait FROM evenement where id=$id";
+                $sql = "SELECT nom,etudiantSatisfait,etudiantNeutre,etudiantInsatisfait FROM evenement where id=$id";
                 $conn->query('SET NAMES utf8');
 
                 //L'action la query est ici
@@ -83,7 +80,8 @@ session_start();
                 if ($result->num_rows > 0) {
 
                     while ($row = $result->fetch_assoc()) {
-                ?>
+        ?>
+                        <span id="titreE" class="visually-hidden"><?php echo $row['nom'] ?></span>
                         <span id="nbEtudSatisf" class="visually-hidden"> <?php echo $row['etudiantSatisfait'] ?></span>
                         <span id="nbEtudNeutre" class="visually-hidden"><?php echo $row['etudiantNeutre'] ?></span>
                         <span id="nbEtudInsatisf" class="visually-hidden"><?php echo $row['etudiantInsatisfait'] ?></span>
@@ -101,50 +99,62 @@ session_start();
 
                     while ($row = $result->fetch_assoc()) {
                     ?>
+
                         <span id="nbEntSatisf" class="visually-hidden"><?php echo $row['employeurSatisfait'] ?></span>
                         <span id="nbEntNeutre" class="visually-hidden"><?php echo $row['employeurNeutre'] ?></span>
                         <span id="nbEntInsatisf" class="visually-hidden"><?php echo $row['employeurInsatisfait'] ?></span>
-                <?php
+        <?php
                     }
                 }
-            }
-            else{
+            } else {
                 header("Location: ./index.php");
             }
-        }
-        else{
+        } else {
             header("Location: ./connexion.php");
         }
         ?>
+
+        <h1 class="text-center" id="titre-event"></h1>
         <!-- Pour avoir un bon comportement responsive, il faut ajouter les styles ci-dessous
              dans le conteneur du "canvas" -->
 
-        <div class="d-flex justify-content-center ">
-            <div class="me-5">
-                <h1 id="titre-etu"></h1>
-                <div class="position-relative ">
+        <div class="d-flex justify-content-center mt-5 canvas-container ">
+            <div class="me-5 text-center">
+                <h2 id="titre-etu"></h2>
+                <div class="position-relative">
                     <canvas id="canvas-diagramme"></canvas>
                 </div>
+                <span  class="fs-4" id="totalVotesEt"></span>
             </div>
-            <div class="ms-5">
-                <h1 id="titre-ent"></h1>
+            <div class="ms-5 text-center">
+                <h2 id="titre-ent"></h2>
                 <div class="position-relative">
                     <canvas id="canvas-diagramme2"></canvas>
                 </div>
+                <span class="fs-4" id="totalVotesEn"></span>
             </div>
         </div>
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <script src="js/statistique.js"></script>
+    <footer class="text-center mt-5">
+        <!-- Copyright -->
+        <p>
+            © 2023 Copyright: Nicolas Fleurent & Mirolie Théroux
+        </p>
+    </footer>
+
 
     <!-- Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <!-- Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-
+    
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="js/statistique.js"></script>
 </body>
 
 </html>
