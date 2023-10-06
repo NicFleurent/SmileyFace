a186f71a5de9b5fa0290a3ef630d54846a70ed3a
Nicolas Fleurent <nicolas.fleurent.02@edu.cegeptr.qc.ca>
2023-10-06 07:52:34 -0400

    connexion serveur

--- connexion.php
+++ connexion.php
@@ -1,8 +1,8 @@
 <?php
 //Démarre la session
 session_start();
-// require("connexionServeur.php");
-require("connexionLocal.php");
+require("connexionServeur.php");
+//require("connexionLocal.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -86,7 +86,7 @@ require("connexionLocal.php");
                 if ($result->num_rows > 0) {
                     $row = $result->fetch_assoc();
                     $_SESSION["connexion"] = true;
-                    $_SESSION["serveur"] = false;
+                    $_SESSION["serveur"] = true;
                     mysqli_close($conn);
                     header("Location: index.php");
                 } else if ($_POST['usager'] != null && $_POST['mdp'] != null) {
